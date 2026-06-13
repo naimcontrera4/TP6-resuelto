49134ed58d46e72c1e769337ff7cb879cf32f9ff
Contrera Ocampo Naim Jair <naimcontrera4@gmail.com>
2026-06-13 12:02:26 -0300

    Ejercicio 4 Resuelto

--- src/main/java/ar/edu/unlar/prog3/tp_comparable_comparator/ui.java
+++ src/main/java/ar/edu/unlar/prog3/tp_comparable_comparator/ui.java
@@ -1,9 +1,7 @@
 package ar.edu.unlar.prog3.tp_comparable_comparator;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
-
 import ar.edu.unlar.prog3.clases.Estudiante;
 
 public class ui {
@@ -14,9 +12,25 @@ public class ui {
         estudiantes.add(new Estudiante("LU-2024-003", "Facundo Castro",     7.2, 24, 22));
         estudiantes.add(new Estudiante("LU-2024-004", "Camila Torres",      9.1, 21, 24));
         estudiantes.add(new Estudiante("LU-2024-010", "Lucía Fernández",    7.8, 21, 16));
-        Collections.sort(estudiantes);
-        for (Estudiante estudiante : estudiantes) {
-            System.out.println(estudiante);
+        Comparator<Estudiante>compMateriasAprobadas=(e1, e2) -> Integer.compare(e1.getMateriasAprobadas(), e2.getMateriasAprobadas());
+        Comparator<Estudiante>compNombre=Comparator.comparing(Estudiante::getNombre);
+        Comparator<Estudiante>compEdad=Comparator.comparing(Estudiante::getEdad);
+        System.out.println("--- ORDEN POR MATERIAS APROBADAS (Ascendente) ---");
+        estudiantes.sort(compMateriasAprobadas);
+        for (Estudiante e : estudiantes) {
+            System.out.println(e); 
+        }
+
+        System.out.println("\n--- ORDEN POR NOMBRE (Alfabético A-Z) ---");
+        estudiantes.sort(compNombre);
+        for (Estudiante e : estudiantes) {
+            System.out.println(e);
+        }
+
+        System.out.println("\n--- ORDEN POR EDAD (Ascendente) ---");
+        estudiantes.sort(compEdad);
+        for (Estudiante e : estudiantes) {
+            System.out.println(e);
         }
     }
 }
